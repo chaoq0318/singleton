ad8671224e9696c55539b8d0f1285004b2a38920
chaoq <chaoq@vmware.com>
2019-11-15 13:39:00 +0800

    add unit test

--- g11n-ws/vip-manager-lite-i18n/src/test/java/com/vmware/vip/i18n/config/SpringBootTest.java
+++ g11n-ws/vip-manager-lite-i18n/src/test/java/com/vmware/vip/i18n/config/SpringBootTest.java
@@ -1,5 +1,6 @@
 package com.vmware.vip.i18n.config;
 
+import com.vmware.vip.common.constants.ConstantsTomcat;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,11 +12,16 @@ import com.vmware.vip.LiteBootApplication;
 public class SpringBootTest {
 	private static Logger logger = LoggerFactory.getLogger(SpringBootTest.class);
 
+	private String[] schemes = {ConstantsTomcat.HTTP_HTTPS/*, ConstantsTomcat.HTTP, ConstantsTomcat.HTTPS*/};
+
 	@Test
 	public void test001serviceswich() {
 		String[] args = { "-c" };
 		try {
-		    LiteBootApplication.main(args);
+			for (String sceme : schemes) {
+				System.setProperty("server.scheme", sceme);
+				LiteBootApplication.main(args);
+			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			logger.error(e.getMessage(), e);
